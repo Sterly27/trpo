b63d557b4e57f7526b65185086f2bd66f5a11909
Огнев Юрий Юрьевич <ognevyury.nsk@gmail.com>
2016-02-29 15:38:33 +0700

    Создание входной функции

---
+++ crypt.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <locale>
+#include <windows.h>
+#include <fstream>
+#include <ctime>
+
+using namespace std;
+const int N=1024;
+
+int main()
+{
+  setlocale(LC_ALL,"Russian");
+  system("PAUSE");
+}
