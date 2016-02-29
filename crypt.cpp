1100826408af706adad3fcec8ab640d37dd7de28
Огнев Юрий Юрьевич <ognevyury.nsk@gmail.com>
2016-02-29 15:41:27 +0700

    Добавлена функция ввода сообщения

--- crypt.cpp
+++ crypt.cpp
@@ -8,9 +8,37 @@
 
 using namespace std;
 const int N=1024;
-
+void msg();
 int main()
 {
   setlocale(LC_ALL,"Russian");
   system("PAUSE");
+}
+
+void msg()
+{
+	char string[N];
+	int key;
+	cout<<"Enter message"<<endl;
+	cout<<"> ";
+	gets(string);
+	cout<<endl;
+	cout<<"Enter key(int number)"<<endl;
+	cout<<"> ";
+	cin>>key;
+	scr(string,key);
+	cout<<"> ";
+	ofstream crypt("crypt.txt",ios_base::app);
+	crypt<<"> "<<string<<endl;
+	crypt.close();
+	cout<<string<<endl;
+	/*
+	Â ñëó÷àå, åñëè ïîíàäîáèòñÿ ñðàçó ðàñøèôðîâàòü
+	*********************************
+	scr(string,key);
+	cout<<"> ";
+	cout<<string<<endl;
+	*********************************
+	*/
+	return;
 }
