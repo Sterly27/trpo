a3b4a1888261f740dd1a1d4f1082e7692cb3916e
DeyMass <silkin.mixail@yandex.ru>
2016-03-15 19:25:27 +0600

    Морзе Новая Нормальная

--- crypt.cpp
+++ crypt.cpp
@@ -25,50 +25,192 @@ putch(7);
 delay(621);
      }
      
-void morze(char* str){
+char* morze(char* str){
+     char* total;
+     
+     total=new char (4*strlen(str));
+     
+printf("%i",strlen(str));     
+
+     int j=0;
      for(int i=0;i<strlen(str);i++){
-     if (str[i]=='A'||str[i]=='a'){printf("*-|");tchk();tire();}
-     if (str[i]=='B'||str[i]=='b'){printf("-***|");tire();tchk();tchk();tchk();}
-     if (str[i]=='C'||str[i]=='c'){printf("-*-*|");tire();tchk();tire();tchk();}
-     if (str[i]=='D'||str[i]=='d'){printf("-**|");tire();tchk();tchk();}
-     if (str[i]=='E'||str[i]=='e'){printf("*|");tchk();}
-     if (str[i]=='F'||str[i]=='f'){printf("**-*|");tchk();tchk();tire();tchk();}
-     if (str[i]=='G'||str[i]=='g'){printf("--*|");tire();tire();tchk();}
-     if (str[i]=='H'||str[i]=='h'){printf("****|");tchk();tchk();tchk();tchk();}
-     if (str[i]=='I'||str[i]=='i'){printf("**|");tchk();tchk();}
-     if (str[i]=='J'||str[i]=='j'){printf("*---|");tchk();tire();tire();tire();}
-     if (str[i]=='K'||str[i]=='k'){printf("-*-|");tire();tchk();tire();}
-     if (str[i]=='L'||str[i]=='l'){printf("*-**|");tchk();tire();tchk();tchk();}
-     if (str[i]=='M'||str[i]=='m'){printf("--|");tire();tire();}
-     if (str[i]=='N'||str[i]=='n'){printf("-*|");tire();tchk();}
-     if (str[i]=='O'||str[i]=='o'){printf("---|");tire();tire();tire();}
-     if (str[i]=='P'||str[i]=='p'){printf("*--*|");tchk();tire();tire();tchk();}
-     if (str[i]=='Q'||str[i]=='q'){printf("--*-|");tire();tire();tchk();tire();}
-     if (str[i]=='R'||str[i]=='r'){printf("*-*|");tchk();tire();tchk();}
-     if (str[i]=='S'||str[i]=='s'){printf("***|");tchk();tchk();tchk();}
-     if (str[i]=='T'||str[i]=='t'){printf("-|");tire();}
-     if (str[i]=='Y'||str[i]=='y'){printf("-*--|");tire();tchk();tire();tire();}
-     if (str[i]=='U'||str[i]=='u'){printf("**-|");tchk();tchk();tire();}
-     if (str[i]=='V'||str[i]=='v'){printf("***-|");tchk();tchk();tchk();tire();}
-     if (str[i]=='W'||str[i]=='w'){printf("*--|");tchk();tire();tire();}
-     if (str[i]=='X'||str[i]=='x'){printf("-**-|");tire();tchk();tchk();tire();}
-     if (str[i]=='Z'||str[i]=='z'){printf("--**|");tire();tire();tchk();tchk();}
-     delay(621);
+     if (str[i]=='A'||str[i]=='a'){
+     total[j++]='*';
+     total[j++]='-';
+     tchk();
+     tire();
      }
+     if (str[i]=='B'||str[i]=='b'){
+     total[j++]='-';
+     total[j++]='*';
+     total[j++]='*';
+     total[j++]='*';
+     tire();
+     tchk();
+     tchk();
+     tchk();
+     }
+     if (str[i]=='C'||str[i]=='c'){
+     total[j++]='-';
+     total[j++]='*';
+     total[j++]='-';
+     total[j++]='*';
+     tire();
+     tchk();
+     tire();
+     tchk();
+     }
+     if (str[i]=='D'||str[i]=='d'){
+     total[j++]='-';
+     total[j++]='*';
+     total[j++]='*';
+     tire();
+     tchk();
+     tchk();
+     }
+     if (str[i]=='E'||str[i]=='e'){
+     total[j++]='*';
+     tchk();}
+     if (str[i]=='F'||str[i]=='f'){total[j++]='*';
+     total[j++]='*';
+     total[j++]='*';
+     total[j++]='-';
+     total[j++]='*';
+     tchk();
+     tchk();
+     tire();
+     tchk();
+     }
+     if (str[i]=='G'||str[i]=='g'){
+     total[j++]='*';
+     total[j++]='-';
+     total[j++]='*';
+     tire();tire();tchk();}
+     if (str[i]=='H'||str[i]=='h'){
+     total[j++]='*';
+     total[j++]='*';
+     total[j++]='*';
+     total[j++]='*';
+     tchk();tchk();tchk();tchk();}
+     if (str[i]=='I'||str[i]=='i'){
+     total[j++]='*';
+     total[j++]='*';
+     tchk();tchk();}
+     if (str[i]=='J'||str[i]=='j'){
+     total[j++]='*';
+     total[j++]='-';
+     total[j++]='-';
+     total[j++]='-';
+     tchk();tire();tire();tire();}
+     if (str[i]=='K'||str[i]=='k'){
+     total[j++]='-';
+     total[j++]='*';
+     total[j++]='-';
+     tire();tchk();tire();}
+     if (str[i]=='L'||str[i]=='l'){
+     total[j++]='*';
+     total[j++]='-';
+     total[j++]='*';
+     total[j++]='*';
+     tchk();tire();tchk();tchk();}
+     if (str[i]=='M'||str[i]=='m'){
+     total[j++]='-';
+     total[j++]='-';
+     tire();tire();}
+     if (str[i]=='N'||str[i]=='n'){
+     total[j++]='-';
+     total[j++]='*';
+     tire();tchk();}
+     if (str[i]=='O'||str[i]=='o'){
+     total[j++]='-';
+     total[j++]='-';
+     total[j++]='-';
+     
+     tire();tire();tire();}
+     if (str[i]=='P'||str[i]=='p'){
+     total[j++]='*';
+     total[j++]='-';
+     total[j++]='-';
+     total[j++]='*';
+     tchk();tire();tire();tchk();}
+     if (str[i]=='Q'||str[i]=='q'){
+     total[j++]='-';
+     total[j++]='-';
+     total[j++]='*';
+     total[j++]='-';
+     tire();tire();tchk();tire();}
+     if (str[i]=='R'||str[i]=='r'){
+     total[j++]='*';
+     total[j++]='-';
+     total[j++]='*';
+     tchk();tire();tchk();}
+     if (str[i]=='S'||str[i]=='s'){
+     total[j++]='*';
+     total[j++]='*';
+     total[j++]='*';
+     tchk();tchk();tchk();}
+     if (str[i]=='T'||str[i]=='t'){
+     total[j++]='*';
+     tire();}
+     if (str[i]=='Y'||str[i]=='y'){
+     total[j++]='-';
+     total[j++]='*';
+     total[j++]='-';
+     total[j++]='-';
+     tire();tchk();tire();tire();}
+     if (str[i]=='U'||str[i]=='u'){
+     total[j++]='*';
+     total[j++]='*';
+     total[j++]='-';
+     tchk();tchk();tire();}
+     if (str[i]=='V'||str[i]=='v'){
+     total[j++]='*';
+     total[j++]='*';
+     total[j++]='*';
+     total[j++]='-';
+     tchk();tchk();tchk();tire();}
+     if (str[i]=='W'||str[i]=='w'){
+     total[j++]='*';
+     total[j++]='-';
+     total[j++]='-';
+     tchk();tire();tire();}
+     if (str[i]=='X'||str[i]=='x'){
+     total[j++]='-';
+     total[j++]='*';
+     total[j++]='*';
+     total[j++]='-';
+     tire();tchk();tchk();tire();}
+     if (str[i]=='Z'||str[i]=='z'){
+     total[j++]='-';
+     total[j++]='-';
+     total[j++]='*';
+     total[j++]='*';
+     tire();tire();tchk();tchk();}
+    if (str[i]==' ') delay(3726);
+     
+     delay(621);
+     total[j++]='|';
+     }
+     total[j++]=0;
      printf("\n------------------Crypted successfully!-------------------\n");
+     return total;
 }
 
 void msg();
 void crpt(char* string,int key);
 int main()
 {
-    char a[100];
+    char a[100],*itog;
+    
     scanf("%s",a);
-    morze(a);
+    
+    itog=morze(a);
+    
+    printf("%s\n",itog);
   setlocale(LC_ALL,"Russian");
   system("PAUSE");
 }
-
+/*
 void msg()
 {
 	char string[N];
@@ -80,7 +222,7 @@ void msg()
 	cout<<"Enter key(int number)"<<endl;
 	cout<<"> ";
 	cin>>key;
-	scr(string,key);
+	//(string,key);
 	cout<<"> ";
 	ofstream crypt("crypt.txt",ios_base::app);
 	crypt<<"> "<<string<<endl;
@@ -96,3 +238,4 @@ void crpt(char* string,int key)
 	return;
 }
 
+*/
