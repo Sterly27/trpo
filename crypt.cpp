f507499b2ac3bea17bf37376bc4e07fb9769a62d
Огнев Юрий Юрьевич <ip513s16@awp08.csc.local>
2016-04-06 08:14:40 +0600

    update crypt

--- crypt.cpp
+++ crypt.cpp
@@ -38,8 +38,12 @@ void msg()
 }
 
 void crpt(char* string,int key)
-{
+{ 
 	for (int i =0; i<strlen(string);++i)
+	{
+		if(string[i]<key) string[i]+=95
 		string[i]^=key;
+		if(string[i]>126) string[i]-=95;
+	}
 	return;
 }
--- func_crpt.cpp
+++ func_crpt.cpp
@@ -1,6 +1,10 @@
 void crpt(char* string,int key)
-{
+{ 
 	for (int i =0; i<strlen(string);++i)
+	{
+		if(string[i]<key) string[i]+=95
 		string[i]^=key;
+		if(string[i]>126) string[i]-=95;
+	}
 	return;
 }
