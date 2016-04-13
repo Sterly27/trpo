1b88e136818dde85ed61264941af7cc76abfd90e
DeyMass <silkin.mixail@yandex.ru>
2016-04-13 15:50:34 +0600

    Revert "WHYYYY?!"
    This reverts commit ac0a4af6a17918ea0f3ab3ebe72c3c1fc6d38215, reversing
    changes made to ccb17b0ad07a4058f5fb112e1a8d6bf466423a4e.

--- test/main.cpp
+++ test/main.cpp
@@ -1,16 +1,8 @@
 #define CTEST_MAIN
-<<<<<<< HEAD
 #include <../src/func.h>
 #include <ctest.h>
 #include <test.cpp>
 
 int main(int argc, const char** argv){
   return ctest_main(argc,argv);
-=======
-
-#include <ctest.h>
-
-int main(int argc, const char** argv) {
-    return ctest_main(argc, argv);
->>>>>>> master
 }
