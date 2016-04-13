8584f78f737e5929678cfa7d64d242954a74d0fe
Sanin <mongstruos@mail.ru>
2016-04-13 09:42:20 +0700

    Update func_crpt_vigi.cpp

--- func_crpt_vigi.cpp
+++ func_crpt_vigi.cpp
@@ -5,37 +5,35 @@
 
 const int N=1000;
 
-void vig();
+char *vig(char *a,char *b);
 
 int main (){
 SetConsoleCP(1251); SetConsoleOutputCP(1251);
 char str[N], str1[N];
-vig();
+vig(str,str1);
 system("PAUSE");
 }
 
-void vig(){
-char a[N], b[N];
+char *vig(char *a,char *b)
+{
 int i=0,j=0;
-gets (a);
-
-gets (b); 
 
  for (i=0;i<strlen(b);i++)
  {
- if(b[i]<(-85))
- b[i]+=98;
- b[i]-=40;
-}
+ b[i]-=31;
+ }
 
 i=0;
  while(a[i]!=0)
  {if(j>=strlen(b))
  j=0;
  a[i]^=b[j];
-
+ if(a[i]<b[j]+31)
+ a[i]=a[i]+95;
+ if(a[i]==127 || a[i]<0)
+     a[i]=a[i]-95;
  i++;j++;
  }  
-puts (a); 
-
+for(i=0;b[i]!=0;i++)
+      b[i]+=31;  
  }
