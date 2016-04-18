cfbd0bc513af61a633003eb8d9568508a28d33ae
Иван Санин < >
2016-04-18 23:41:39 +0600

    Преобразование одной функции в две с целью повышения надежности

--- src/func_crpt_vigi.cpp
+++ src/func_crpt_vigi.cpp
@@ -1,22 +1,38 @@
-
-void vig(char *a,char *b)
-{
+void vig(char a[],char b[],int flag){
 int i=0,j=0;
- for (i=0;i<strlen(b);i++)
- {
- b[i]-=31;
- }
+for (i=0;i<strlen(b);i++){
+ b[i]=b[i]-29;
+}
 i=0;
- while(a[i]!=0)
- {if(j>=strlen(b))
+ while(a[i]!=0){
+ if(b[j]==0)
  j=0;
- a[i]^=b[j];
- if(a[i]<b[j]+31)
- a[i]=a[i]+95;
+ a[i]=a[i]+b[j];
  if(a[i]==127 || a[i]<0)
      a[i]=a[i]-95;
+ if(a[i]<32)
+     a[i]=a[i]+95;
  i++;j++;
- }  
+ }     
 for(i=0;b[i]!=0;i++)
-      b[i]+=31;  
+      b[i]+=29;                
+}
+
+
+void devig(char a[],char b[],int flag){
+int i=0,j=0;
+for (i=0;i<strlen(b);i++){
+ b[i]=b[i]-29;
+}
+i=0;
+ while(a[i]!=0){
+ if(b[j]==0)
+ j=0;
+  a[i]=a[i]-b[j];
+ if(a[i]<b[j]+29)
+ 	a[i]=a[i]+95;
+ i++;j++;
  }
+for(i=0;b[i]!=0;i++)
+      b[i]+=29;                
+}
