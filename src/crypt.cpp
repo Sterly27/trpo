c4a7b448730ade01e0cc879228f2f908ff9f3baf
AziRo <pashka0989@yandex.ru>
2016-04-24 19:37:02 +0600

    input\output->cin\cout

--- src/crypt.cpp
+++ src/crypt.cpp
@@ -2,6 +2,9 @@
 #include "crypt_func.h"
 #include <stdio.h>
 #include <string.h>
+#include <iostream>
+
+using namespace std;
 
 const int N=1024;
 
@@ -24,137 +27,130 @@ int main(){
 	    } 
 	    //---------------------------------------END_RESET-----------------------------------//
 	    system("cls||clear");
-	    printf("1)Encrypt\n\n");
-	    printf("2)Decrypt\n\n");
-	    printf("3)Exit\n\n");
+	    cout<<"1)Encrypt\n\n";
+	    cout<<"2)Decrypt\n\n";
+	    cout<<"3)Exit\n\n";
 		scanf("%i",&menu);
 		switch(menu){
 			case 1:
 				while(menu!=4){
 					system("cls||clear");
-					printf("1)Vigener\n\n");
-					printf("2)Caesar\n\n");
-					printf("3)Morze\n\n");
-					printf("4)Back\n\n");
+					cout<<"1)Vigener\n\n";
+					cout<<"2)Caesar\n\n";
+					cout<<"3)Morze\n\n";
+					cout<<"4)Back\n\n";
 					scanf("%i",&menu);
 					switch(menu){
 						case 1://--------------------CRYPT_VIGI-------------------//
 							system("cls||clear");
-							printf("Enter text:\n");
-							gets(str);
-							gets(str);
-							puts("Enter key:");
-							gets(key1);
-							if(vig(str,key1) > 0)
-                     {
-                        printf("Error: invalid symbols.");
-                        break;
-                     } else
-                     {
-                         if(vig(str,key1) < 0)
-                         {
-                            printf("Error: string/key is empty.");
-                            break;                 
-                         }    
-                     } 
-							puts("Encrypted text:");
-							puts(str);
-							gets(paus);
+							cout<<"Enter text:\n";
+							cin.getline(str,N);
+							cin.getline(str,N);
+							cout<<"Enter text:\n";
+							cin.getline(key1,N);
+							if(vig(str,key1) > 0){
+								system("cls||clear");
+								cout<<"Error: invalid symbols.";
+								cin>>paus;
+								break;
+							} 
+							else if(vig(str,key1) < 0){
+								system("cls||clear");
+								cout<<"Error: string/key is empty.";
+								cin>>paus;
+								break;                 
+							} 
+							cout<<"Encrypted text:\n" << str;
+							
+							cin>>paus;
 							break;
 						case 2://-------------------CRYPT_CEASER------------------//
 							system("cls||clear");
-							printf("Enter text:\n");
-							gets(str);
-							gets(str);
-							printf("Enter key:\n");
+							cout<<"Enter text:\n";
+							cin.getline(str,N);
+							cin.getline(str,N);
+							cout<<"Enter key:\n";
 							scanf("%i",&key2);
 							crpt(str,key2);
-							printf("Encrypted text:\n");
-							puts(str);
-							gets(paus);
+							cout<<"Encrypted text:\n" << str;
+							cin>>paus;
 							break;
 						case 3://-------------------CRYPT_MORZE------------------//
 							system("cls||clear");
-							printf("Enter text:\n");
-							gets(str);
-							gets(str);
+							cout<<"Enter text:\n";
+							cin.getline(str,N);
+							cin.getline(str,N);
 							strm=new char [strlen(str)*10];
 							morze(str,strm);
-							printf("Encrypted text:\n");
-							puts(strm);
-							gets(paus);
+							cout<<"Encrypted text:\n" << strm;
+							cin>>paus;
 							break;
 						case 4://-------------------BUTTON_BACK------------------//
 							break;
 						default://----------------OTHER_OPTIONS-----------------//
 							system("cls||clear");
-							printf("This menu item does not exist.\n\n");
-							gets(paus);
-							gets(paus);
+							cout<<"This menu item does not exist.\n\n";
+							cin>>paus;
 					}
 				}
 				break;
 			case 2:
 				while(menu!=4){
 					system("cls||clear");
-					printf("1)Vigener\n\n");
-					printf("2)Caesar\n\n");
-					printf("3)Morze\n\n");
-					printf("4)Back\n\n");
+					cout<<"1)Vigener\n\n";
+					cout<<"2)Caesar\n\n";
+					cout<<"3)Morze\n\n";
+					cout<<"4)Back\n\n";
 					scanf("%i",&menu);
 					switch(menu){
 						case 1://-------------------DECRYPT_VIGI------------------//
 							system("cls||clear");
-							printf("Enter text:\n");
-							gets(str);
-							gets(str);
-							printf("Enter key:\n");
-							gets(key1);
-							if(devig(str,key1) > 0)
-                     {
-                        printf("Error: invalid symbols.");
-                        break;
-                     } else
-                     {
-                         if(devig(str,key1) < 0)
-                         {
-                            printf("Error: string/key is empty.");
-                            break;                 
-                         }    
-                     }               
-							printf("Decrypted text:\n");
-							puts(str);
-							gets(paus);
+							cout<<"Enter text:\n";
+							cin.getline(str,N);
+							cin.getline(str,N);
+							cout<<"Enter key:\n";
+							cin.getline(key1,N);
+							if(devig(str,key1) > 0){
+								system("cls||clear");
+								cout<<"Error: invalid symbols.";
+								cin>>paus;
+								break;
+							}
+							else if(devig(str,key1) < 0){
+								system("cls||clear");
+								cout<<"Error: string/key is empty.";
+								cin>>paus;
+								break;                 
+							}        
+							cout<<"Decrypted text:\n" << str;
+							cin>>paus;
 							break;
 						case 2://------------------DECRYPT_CEASER-----------------//
 							system("cls||clear");
-							printf("Enter text:\n");
-							gets(str);
-							gets(str);
-							printf("Enter key:\n");
+							cout<<"Enter text:\n";
+							cin.getline(str,N);
+							cin.getline(str,N);
+							cout<<"Enter key:\n";
 							scanf("%i",&key2);
 							crpt(str,key2);
-							printf("Decrypted text:\n");
-							puts(str);
-							gets(paus);
+							cout<<"Decrypted text:\n" << str;
+							cin>>paus;
 							break;
 						case 3://------------------DECRYPT_MORZE-----------------//
 							system("cls||clear");
-							printf("Enter text:\n");
-							gets(str);
-							gets(str);
+							cout<<"Enter text:\n";
+							cin.getline(str,N);
+							cin.getline(str,N);
 							demorze(str);
-							printf("Encrypted text:\n");
-							puts(str);
-							gets(paus);
+							cout<<"Decrypted text:\n" << str;
+							cin>>paus;
 							break;
 						case 4://------------------BUTTON_BACK-------------------//
 							break;
 						default://----------------OTHER_OPTIONS-----------------//
 							system("cls||clear");
-							printf("This menu item does not exist.\n\n");
-							gets(paus);
-							gets(paus);
+							cout<<"This menu item does not exist.\n\n";
+							cin>>paus;
 					}
 				}
 				break;
@@ -164,15 +160,13 @@ int main(){
 				break;
 			case 45:
 			    system("cls||clear");
-				printf("Group IP-513 thanks you for using our software!");
-				gets(paus);
-				gets(paus);
+				cout<<"Group IP-513 thanks you for using our software!";
+				cin>>paus;
 				break;
 			default://------------------------OTHER_OPTIONS--------------------------//
 				system("cls||clear");
-				printf("This menu item does not exist.\n\n");
-				gets(paus);
-				gets(paus);
+				cout<<"This menu item does not exist.\n\n";
+				cin>>paus;
 				break;	
 		}
 	}
--- src/crypt_func.h
+++ src/crypt_func.h
@@ -2,8 +2,8 @@
 #define CRYPT_FUNC_H
 
 int crpt(char* string,int key);
-void vig(char a[],char b[]);
-void devig(char a[],char b[]);
+int vig(char a[],char b[]);
+int devig(char a[],char b[]);
 void morze(char* str, char* total);
 void demorze(char* str);
 
