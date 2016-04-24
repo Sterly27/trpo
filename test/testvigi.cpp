27d8bc67a9a4c59f8f9aea5341a07d7ac7391cc7
Monstruos <mongstruos@mail.ru>
2016-04-24 22:22:26 +0600

    added 2 new tests

--- test/testvigi.cpp
+++ test/testvigi.cpp
@@ -1,11 +1,12 @@
 #include <string.h>
 #include "../src/crypt_func.h"
+
 CTEST (vigi,test1ver1){
       // General operability
-char a[]=" !\"#$%%&'()*+,-./0123456789:;<=>?@ABC";
+char a[]=" !##$'&'()*+,-./0123456789:;<=>?@ABC";
 char b[]=" !\"";
 vig (a,b);
-char d[]="!#%%$&(')+*,.-/102435768:9;=<>@?ACBDF";
+char d[]="!#&$&*')+*,.-/102435768:9;=<>@?ACBDF";
 assert_str(d,a,"",1);
 }
 
@@ -23,13 +24,13 @@ CTEST (vigi,test2){
 char a[]="o";
 char b[]="o";
 vig (a,b);
-char d[]=" ";
+char d[]="'";
 assert_str(d,a,"",1);
 }
 
 CTEST (devigi,test3){
       //double addition
-char a[]=" ";
+char a[]="'";
 char b[]="o";
 devig (a,b);
 char d[]="o";
@@ -38,23 +39,23 @@ assert_str(d,a,"",1);
 
 CTEST (vigi,test4){
       //max result of substraction
-char a[]=")";
+char a[]="~";
 char b[]="~";
 vig (a,b);
 char d[]="~";
 assert_str(d,a,"",1);
 }
 
-CTEST (vigi,test3){
+CTEST (devigi,test3){
       //max result of addition
 char a[]="~";
 char b[]="~";
-vig (a,b);
-char d[]=")";
+devig (a,b);
+char d[]="~";
 assert_str(d,a,"",1);
 }
 
-CTEST (vigi,test5ver5){
+CTEST (devigi,test5ver5){
       // General operability
 char a[]=" !\"#$%%&'()*+,-./0123456789:;<=>?@ABC";
 char b[]=" !\"";
@@ -63,11 +64,29 @@ devig (d,b);
 assert_str(a,d,"",1);
 }
 
-CTEST (vigi,test5ver2){
+CTEST (devigi,test5ver2){
       // General operability
 char a[]="DEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~";
 char b[]=" !\"";
 char d[]="EGIHJLKMONPRQSUTVXWY[Z\\^]_a`bdcegfhjikmlnpoqsrtvuwyxz|{} ~!";
 devig (d,b);
 assert_str(a,d,"",1);
+}
+
+CTEST (vigi,test6){
+      //max result of substraction
+char a[]="%%";
+char b[]="\"";
+vig (a,b);
+char d[]="(";
+assert_str(d,a,"",1);
+}
+
+CTEST (devigi,testde6){
+      //max result of addition
+char a[]="(";
+char b[]="\"";
+devig (a,b);
+char d[]="%%";
+assert_str(d,a,"",1);
 }
