995298d2610af866dda5963063a9a404c7c0cf19
Fedotov Pavel <pashka0989@yandex.ru>
2016-04-29 07:22:09 +0600

    +Tests

---
+++ test/main.cpp
@@ -0,0 +1,9 @@
+#define CTEST_MAIN
+//#include "distance_test.cpp"
+#include "../thirdparty/ctest.h"
+#include "test.cpp"
+#include "testvigi.cpp"
+
+int main(int argc, const char** argv){
+  return ctest_main(argc,argv);
+}
