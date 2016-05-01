830a9172b81278e7eaef9afb5ed0ae6784bdb06d
Monstruos <mongstruos@mail.ru>
2016-05-01 20:58:33 +0600

    fixed crypting with line break

--- src/func_crpt_vigi.cpp
+++ src/func_crpt_vigi.cpp
@@ -5,7 +5,10 @@ int i=0,j=0;
 if((!strlen(a)) || (!strlen(b))) {return -1;}
 for (i=0;i<strlen(a);i++)
 {
-    if((a[i] < 32) || (a[i] == 127)) {return 1;}
+    if((a[i] < 32) || (a[i] == 127) || ) {
+        if(a[i] != 13)
+            return 1;
+    }
 }
 for (i=0;i<strlen(b);i++)
 {
@@ -16,14 +19,17 @@ for (i=0;i<strlen(b);i++){
  b[i]=b[i]-31;
 }
 i=0;
- while(a[i]!=0){
- if(b[j]==0)
- j=0;
- a[i]=a[i]+b[j];
- while(a[i]==127 || a[i]<32)
-     a[i]=a[i]-95;
- i++;j++;
- }     
+ while(a[i] != 0){
+     if(b[j] == 0)
+     j=0;
+     if(a[i] != 13){
+        a[i] = a[i] + b[j];
+        while(a[i] == 127 || a[i] < 32)
+                a[i] = a[i] - 95;
+        j++;
+    }
+    i++;
+}
 for(i=0;b[i]!=0;i++)
       b[i]+=31; 
       return 0;               
@@ -35,7 +41,10 @@ int i=0,j=0;
 if((!strlen(a)) || (!strlen(b))) {return -1;}
 for (i=0;i<strlen(a);i++)
 {
-    if((a[i] < 32) || (a[i] == 127)) {return 1;}
+    if((a[i] < 32) || (a[i] == 127)) {
+        if(a[i] != 13)
+            return 1;
+    }
 }
 for (i=0;i<strlen(b);i++)
 {
@@ -45,14 +54,17 @@ for (i=0;i<strlen(b);i++){
  b[i]=b[i]-31;
 }
 i=0;
- while(a[i]!=0){
- if(b[j]==0)
- j=0;
-  a[i]=a[i]-b[j];
-while(a[i]==127 || a[i]<32)
- 	a[i]=a[i]+95;
- i++;j++;
- }
+while(a[i]!=0){
+    if(b[j]==0)
+    j=0;
+    if(a[i] != 13){
+       a[i] = a[i] - b[j];
+       while(a[i] == 127 || a[i] < 32)
+               a[i] = a[i] + 95;
+       j++;
+   }
+   i++;
+}
 for(i=0;b[i]!=0;i++)
       b[i]+=31;   
       return 0;             
